7d0f8530280452f2dc35e72b09b03666d1c5980c
willhwj <williamhuwj@gmail.com>
2021-10-23 12:05:11 +0800

    added new routes for create new snippet and update existing snippet and delete snippet

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ let app = express();
 app.use(express.json());
 app.use(cors());
 
-// connet to the Mongo DB
-async function connect(){
+// connect to the Mongo DB
+async function connect() {
     const mongo_url = process.env.MONGO_URI;
     let client = await MongoClient.connect(mongo_url, {
         "useUnifiedTopology": true
@@ -24,12 +24,55 @@ async function connect(){
 
 // Routes
 
-async function main(){
-    // let db = await connect();
+async function main() {
 
-    app.get("/snippets", async(req, res)=> {
-        let snippets= await db.collection("snippets").find().toArray();
+    app.get("/snippets", async (req, res) => {
+        let db = await connect();
+        let snippets = await db.collection("snippets").find().toArray();
         res.json(snippets)
+    })
+
+    app.delete("/snippets/delete/:id", async (req, res) => {
+        let db = await connect();
+        let results = await db.collection("snippets").deleteOne({
+            "_id": ObjectId(req.params.id)
+        });
+        res.status(200);
+        res.send(results);
+    })
+
+    app.patch("/snippets/update/:id", async (req, res) => {
+        let db = await connect();
+        let results = await db.collection("snippets").updateOne({
+            "_id": ObjectId(req.params.id)
+            }, 
+            {$set: {
+                name: req.body.name,
+                content: req.body.content,
+                occasions: [...req.body.occasions],
+                type: req.body.type,
+                theme: req.body.theme,
+                length: req.body.length
+            }},
+            {upsert: true});
+        res.status(200);
+        res.send(results);
+    })
+
+    app.post("/snippets/create", async (req, res)=> {
+        console.log(req.body);
+        let db = await connect();
+        let results = await db.collection("snippets").insertOne({
+            name: req.body.name,
+            creator: {...req.body.creator},
+            content: req.body.content,
+            occasions: [...req.body.occasions],
+            type: req.body.type,
+            theme: req.body.theme,
+            length: req.body.length
+        });
+        res.status(200);
+        res.send(results);
     })
 
 }
@@ -37,6 +80,6 @@ async function main(){
 main();
 
 // Start Server
-app.listen(8888, ()=>{
+app.listen(8888, () => {
     console.log("server has started")
 })
